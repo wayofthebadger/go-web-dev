8414d8f3ad97858730b1dd9da7b2992f038de40b
Tom Seward <tom.seward@forgerock.com>
2022-01-04 17:10:40 +0000

    Adds contact page

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	// fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<h1>Please get in touch!</h1>")
+	}
+	//
+	// fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
